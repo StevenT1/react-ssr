a5bdf8970adb79880704d5b66461028a25229e41
taoqiupo <895665504@qq.com>
2020-11-04 16:31:15 +0800

    修复静态资源无法自出的问题

--- build/webpack.client.js
+++ build/webpack.client.js
@@ -1,31 +1,32 @@
 const path = require('path');
+
 // const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const smp = new SpeedMeasurePlugin();
-// module.exports = smp.wrap({
+
 module.exports = {
     mode: 'development',
-    // cache: {
-    //     type:'filesystem'
-    //   },
+    //入口文件
     entry: path.join(__dirname, '../src/client/index.tsx'),
+    //出口位置
     output: {
         filename: 'scripts/[name].bundle.js',
         path: path.join(__dirname, '../dist/assets'),
     },
     module: {
-        rules: [{
-            test: /\.ts(x)?$/,
-            use: 'babel-loader',
-        }]
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                use: 'babel-loader'
+            }
+        ]
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json', '.mjs']
+    resolve:{
+        //设置如何被解析
+        extensions: ['.ts', '.js', '.json','.tsx']
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.resolve(__dirname, '../src/client/template/index.html'),
+            filename:'temlpate.html',
+            template:path.resolve(__dirname, '../src/client/template/index.html')
         })
     ]
     // })
--- src/server/app.tsx
+++ src/server/app.tsx
@@ -1,51 +1,50 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import serve from 'koa-static';
-import ReactDOMServer, { renderToString } from 'react-dom/server';
 import React from 'react';
-const fs = require('fs');
-const path = require('path');
-import App from '../shared/app';
-import routes from '../shared/Routes';
+import ReactDOMServer, { renderToString } from 'react-dom/server';
 import {
     matchPath,
     StaticRouter
 } from 'react-router-dom';
+import App from '../shared/app';
 import { Provider } from 'react-redux';
-import {createServerStore} from '../shared/store'
-const app = new Koa();
-const router = new Router();
+import { createServerStore } from '../../store'
+import routes from '../shared/Routes';
+
+const fs = require('fs');
+const path = require('path');
 
 //读取模版文件的地址
-const fileResolve = file => path.resolve(__dirname,file);
+const fileResolve = file => path.resolve(__dirname, file);
 //读取到模版文件
-const template  = fs.readFileSync(fileResolve('assets/index.html'),'utf-8')
-function templating(template){
-    return props =>template.replace(/<!--(\w+)-->/g,(_,key)=>props[key.trim()])
+const template = fs.readFileSync(fileResolve('assets/temlpate.html'), 'utf-8')
+function templating(template) {
+    return props => template.replace(/<!--(\w+)-->/g, (_, key) => props[key.trim()])
 }
 
+const app = new Koa();
+//使用路由进行路径管理
+const router = new Router({});
 router.get(['/', '/about'], async (ctx, next) => {
-    const store = createServerStore();
     const promises = [];
-    //只加载当前页面刷新的页面
-    routes.some(route => {
-        // console.log(ctx.request.path)
-        const match = matchPath(ctx.request.path, route);
-        if (match){
-            route.loadData ? promises.push(route.loadData(store)) :null;
-        }
-        return match;
-    });
-    //全部路由的预数据加载出来
-    // routes.forEach((route) => {
-    //     route.loadData ? promises.push(route.loadData(store)):null;
-    // })
+    const store = createServerStore();
 
+    routes.some(route => {
+        const match = matchPath(ctx.request.path, route);
+        if (match && route.loadData) {
+            promises.push(route.loadData(store))
+        }
+        // if (match) {
+        //     route.loadData ? promises.push(route.loadData(store)) : promises.push(new Promise(resolve => resolve(123)))
+        // }
+        return match;
+    })
+//      routes.forEach((route) => {
+//          route.loadData ? promises.push(route.loadData(store)):null;
+//  })
     await Promise.all(promises).then(() => {
-        //得到模版的等待替换函数
-        const render = templating(template);
-        
-        // console.log(store.getState());
+
         const html = ReactDOMServer.renderToString(
             <Provider store={store}>
                 <StaticRouter location={ctx.request.url}>
@@ -53,39 +52,25 @@ router.get(['/', '/about'], async (ctx, next) => {
                 </StaticRouter>
             </Provider>
         );
-        // ctx.body = `<!DOCTYPE html>
-        // <html lang="en">
-        // <head>
-        //     <meta charset="UTF-8">
-        //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //     <title>ssr</title>
-        // </head>
-        // <body>
-        // <script>window.REDUX = ${JSON.stringify(store.getState())} </script>
-        //     <div id='root'>${html}</div>
-        //     <script src="bundle.js"></script>
-            
-        // </body>
-        // </html>`
-        //输入参数，替换html和store内的内容
+        const render = templating(template);
         ctx.body = render({
             html,
-            //脱水的数据
-            // store:`<script>window.REDUX = ${JSON.stringify(store.getState())} </script>`
-            store:`<script>window.REDUX =${JSON.stringify(store.getState())}</script>`
+            store: `<script>window.REDUX =${JSON.stringify(store.getState())}</script>`
         })
     })
-    
+
 })
+
 router.get('/getData', ctx => {
     ctx.body = {
         code: 0,
-        message: "ddd",
-        data: "返回的数据"
+        data: "得到的数据"
     }
 })
+//把静态资源绑到页面上
 app.use(serve('assets'));
+//把路由中间件绑上app
 app.use(router.routes()).use(router.allowedMethods());
-app.listen("8080", () => {
-    console.log("开启服务8080")
+app.listen(7777, () => {
+    console.log('ssr服务已启动于7777端口')
 })
